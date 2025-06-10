5cd8737bfb0dbb2c0c892e3d3ecfd7cbfb387cf5
cclaudiah <claudia.carrascoh@gmail.com>
2025-06-09 20:55:47 -0400

    Primer codigo de python

---
+++ Clase1/ejercicio.py
@@ -0,0 +1,6 @@
+print("Bienvenido al curso de Ing. de Datos")
+nombre = input("¿Cómo te llamas? ")
+edad = input("¿Qué edad tienes? ")
+pais = input("¿De qué país eres? ")
+
+print(f"Hola {nombre}, tienes {edad} años y vives en {pais}")
